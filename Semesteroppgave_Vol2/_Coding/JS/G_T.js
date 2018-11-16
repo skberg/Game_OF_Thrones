a2bc1e0c4de160bd82f731648b63a1a95696346d
Svenn Kristoffer Berg <32140076+skberg@users.noreply.github.com>
2018-11-16 11:26:25 +0100

    update

--- Semesteroppgave_Vol2/_Coding/JS/G_T.js
+++ Semesteroppgave_Vol2/_Coding/JS/G_T.js
@@ -13,8 +13,8 @@ fetch('https://anapioficeandfire.com/api/characters')
   function CardElm(myJson){
     
     
-   
-    var cards = document.createElement("container");
+   // Fix
+    var cards = document.createElement("div");
    
     cards.setAttribute("class", "cardElement");
     
@@ -22,65 +22,88 @@ fetch('https://anapioficeandfire.com/api/characters')
     
     for (var i = 0; i < myJson.length; i++) {
 
-
+ // lager elementene 
         var MyDiv = document.createElement("div");
-        var MyDiv2 = document.createElement("h1");
-        var prag = document.createTextNode("ptest")
-         //h4 
+        var MyHEding = document.createElement("h1");
+        var Myprag= document.createElement("p");
+        var MyImg = document.createElement("img");
+        var mylist = document.createElement("ul");
+        
+        
+        for ( var y = 0; y < 1; y++ ){ 
+        var Mylistitem_Name = document.createElement("li");
+        var Mylistitem_Gend = document.createElement("li");
+        var Mylistitem_Contry = document.createElement("li");
+        var Mylistitem_Born = document.createElement("li");
+        var Mylistitem_titles = document.createElement("li");
+     
+        mylist.appendChild(Mylistitem_Gend);
+        mylist.appendChild(Mylistitem_Contry);
+        mylist.appendChild(Mylistitem_Born);
+        mylist.appendChild(Mylistitem_titles);
+
+
+      
+        }
    
        
 
 
 
+
+ // sette classe navn 
+        MyDiv.setAttribute("class" , "[ cards ]");
+        MyDiv.setAttribute("id",  i);
+      
+        Myprag.setAttribute('class', 'p');
+        MyImg.setAttribute('class', 'img');
+        MyImg.setAttribute('class', "nod" + i);
+        mylist.setAttribute('class','list');
+        Mylistitem_Gend.setAttribute('id', myJson[i].gender + i);
+        Mylistitem_Contry.setAttribute('id', myJson[i].culture + i);
+        Mylistitem_Born.setAttribute('id', myJson[i].born + i);
+        Mylistitem_titles.setAttribute('id', myJson[i].titles + i);
+
+        // mylist.setAttribute('id', myJson[i].gender + i);
+     
+        
+       
+     
+        
+   
+       
+
         
         
 
+// For å putte ting inn i andre ting        
+        MyDiv.appendChild(MyHEding);
+        MyDiv.appendChild(Myprag);
+        MyDiv.appendChild(MyImg);
+    
+        MyDiv.appendChild(mylist);
 
-        Conts.appendChild(cards);
+        cards.appendChild(MyDiv);
         myCont.appendChild(cards);
         
-        cards.appendChild(MyDiv);
-        MyDiv.appendChild(MyDiv2);
-        MyDiv2.appendChild(prag);
-      
-        
     
-       
-        
-
-
-
-
-
-         MyDiv.setAttribute("class" , "[ cards ]", 'id', + i);
-         MyDiv.setAttribute('num', i);
-         MyDiv2.setAttribute('prag', i);
-        
-      
-        
-         
-       
-        console.log(prag);
-      
-       
-        
   
-
-      
+     
         
-      
-        MyDiv.innerHTML = "card" + i;
-        MyDiv.innerHTML = "heDing" + i;
-    
-        MyDiv2.innerHTML = "This is a test card for Noroff it is number  " + i;
-        prag.innerHTML = "hello World";
+   
 
-      
-        
+
+
+      Myprag.innerHTML = myJson[i].aliases;
+  
+      Mylistitem_Gend.innerHTML = myJson[i].gender;
+      Mylistitem_Contry.innerHTML = myJson[i].culture;
+      Mylistitem_Born.innerHTML = myJson[i].born;
+      Mylistitem_titles.innerHTML = myJson[i].titles;
 
     }
   
-   //
+
    
 
   } 
