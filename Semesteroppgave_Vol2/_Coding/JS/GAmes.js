aaa9eb5d5e267adb5f63067f1771c25e7bdd4bbb
Svenn Kristoffer Berg <32140076+skberg@users.noreply.github.com>
2018-11-21 18:23:56 +0100

    update
    Local storage in the browser is added so that the user can choose a house and take it with you to the next page where he / she can move with the help of a cube throwing 6

--- Semesteroppgave_Vol2/_Coding/JS/GAmes.js
+++ Semesteroppgave_Vol2/_Coding/JS/GAmes.js
@@ -1,18 +1,114 @@
+switch (localStorage.getItem("player 1")) {
+    case "House Algood":
+        //var el = document.getElementById("yabanner");
+        //el.innerHTML="<img src=\"./Bilde/Icons/Hand_Dark_Red.svg\" width=\"100%\" height=\"50%\">";
+        break;
+    case "House Allyrion of Godsgrace":
+        //var el = document.getElementById("yabanner");
+        //el.innerHTML="<img src=\"./Bilde/Icons/Hand_Dark_Red.svg\" width=\"100%\" height=\"50%\">";
+        break;
+    case "House Allyrion of Godsgrace":
+        //var el = document.getElementById("yabanner");
+        // el.innerHTML="<img src=\"./Bilde/Icons/Hand_Dark_Red.svg\" width=\"100%\" height=\"50%\">";
+        break;
+    case "House Amber":
+        //var el = document.getElementById("yabanner");
+        //el.innerHTML="<img src=\"./Bilde/Icons/Hand_Dark_Red.svg\" width=\"100%\" height=\"50%\">";
+        break;
+    case "House Ambrose":
+        //var el = document.getElementById("yabanner");
+        //el.innerHTML="<img src=\"./Bilde/Icons/Hand_Dark_Red.svg\" width=\"100%\" height=\"50%\">";
+        break;
+    case "House Appleton of Appleton":
+        //var el = document.getElementById("yabanner");
+        //el.innerHTML="<img src=\"./Bilde/Icons/Hand_Dark_Red.svg\" width=\"100%\" height=\"50%\">";
+        break;
+    case "House Arryn of Gulltown":
+        //var el = document.getElementById("yabanner");
+        //el.innerHTML="<img src=\"./Bilde/Icons/Hand_Dark_Red.svg\" width=\"100%\" height=\"50%\">";
+        break;
+    case "House Arryn of the Eyrie":
+        //var el = document.getElementById("yabanner");
+        //el.innerHTML="<img src=\"./Bilde/Icons/Hand_Dark_Red.svg\" width=\"100%\" height=\"50%\">";
+        break;
+    case "House Ashford of Ashford":
+        //var el = document.getElementById("yabanner");
+        el.innerHTML = "<img src=\"./Bilde/Icons/Hand_Dark_Red.svg\" width=\"100%\" height=\"50%\">";
+        break;
+    case "House Ashwood":
+        //var el = document.getElementById("yabanner");
+        el.innerHTML = "<img src=\"./Bilde/Icons/Hand_Dark_Red.svg\" width=\"100%\" height=\"50%\">";
+        break;
+    case "House Baelish of Harrenhal":
+        //var el = document.getElementById("yabanner");
+        //el.innerHTML="<img src=\"./Bilde/Icons/Hand_Dark_Red.svg\" width=\"100%\" height=\"50%\">";
+        break;
+
+    default:
+        console.log("work, no info")
+
+}
+
+switch (localStorage.getItem("player 2")) {
+    case "House Algood":
+        //      var el = document.getElementById("yabanner");
+        //    el.innerHTML="<img src=\"./Bilde/Icons/Hand_Dark_Red.svg\" width=\"100%\" height=\"50%\">";
+        break;
+    case "House Allyrion of Godsgrace":
+        //var el = document.getElementById("yabanner");
+        //el.innerHTML="<img src=\"./Bilde/Icons/Hand_Dark_Red.svg\" width=\"100%\" height=\"50%\">";
+        break;
+    case "House Allyrion of Godsgrace":
+        //var el = document.getElementById("yabanner");
+        //el.innerHTML="<img src=\"./Bilde/Icons/Hand_Dark_Red.svg\" width=\"100%\" height=\"50%\">";
+        break;
+    case "House Amber":
+        //var el = document.getElementById("yabanner");
+        //el.innerHTML="<img src=\"./Bilde/Icons/Hand_Dark_Red.svg\" width=\"100%\" height=\"50%\">";
+        break;
+    case "House Ambrose":
+        //var el = document.getElementById("yabanner");
+        //el.innerHTML="<img src=\"./Bilde/Icons/Hand_Dark_Red.svg\" width=\"100%\" height=\"50%\">";
+        break;
+    case "House Appleton of Appleton":
+        //var el = document.getElementById("yabanner");
+        //el.innerHTML="<img src=\"./Bilde/Icons/Hand_Dark_Red.svg\" width=\"100%\" height=\"50%\">";
+        break;
+    case "House Arryn of Gulltown":
+        //var el = document.getElementById("yabanner");
+        //el.innerHTML="<img src=\"./Bilde/Icons/Hand_Dark_Red.svg\" width=\"100%\" height=\"50%\">";
+        break;
+    case "House Arryn of the Eyrie":
+        //var el = document.getElementById("yabanner");
+        //el.innerHTML="<img src=\"./Bilde/Icons/Hand_Dark_Red.svg\" width=\"100%\" height=\"50%\">";
+        break;
+    case "House Ashford of Ashford":
+        //var el = document.getElementById("yabanner");
+        //el.innerHTML="<img src=\"./Bilde/Icons/Hand_Dark_Red.svg\" width=\"100%\" height=\"50%\">";
+        break;
+    case "House Ashwood":
+        var el = document.getElementById("yabanner");
+        el.innerHTML = "<img src=\"./Bilde/Icons/Hand_Dark_Red.svg\" width=\"100%\" height=\"50%\">";
+        break;
+    case "House Baelish of Harrenhal":
+
+        console.log(myImage);
+        break;
+
+    default:
+        console.log("work, no info")
+}
 
 
 
 
-function rollDice(){
+
+
+
+
+
+function rollDice() {
     var die1 = document.getElementById("die1");
-    var die2 = document.getElementById("die2");
-    var status = document.getElementById("status");
     var d1 = Math.floor(Math.random() * 6) + 1;
-    var d2 = Math.floor(Math.random() * 6) + 1;
-    var diceTotal = d1 + d2;
     die1.innerHTML = d1;
-    die2.innerHTML = d2;
-    status.innerHTML = "You rolled "+diceTotal+".";
-    if(d1 == d2){
-        status.innerHTML += " DOUBLES! You get a free turn!!";
-    }
 }
