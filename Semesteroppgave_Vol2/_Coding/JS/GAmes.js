041479d53d95500a3965fb3f3fe5e60c7bb1f1d9
Svenn Kristoffer Berg <32140076+skberg@users.noreply.github.com>
2018-11-24 11:17:14 +0100

    update

--- Semesteroppgave_Vol2/_Coding/JS/GAmes.js
+++ Semesteroppgave_Vol2/_Coding/JS/GAmes.js
@@ -1,99 +1,197 @@
 switch (localStorage.getItem("player 1")) {
     case "House Algood":
-        //var el = document.getElementById("yabanner");
-        //el.innerHTML="<img src=\"./Bilde/Icons/Hand_Dark_Red.svg\" width=\"100%\" height=\"50%\">";
+        var el = document.getElementById("player1");
+        el.innerHTML="<img src=\"./Bilde/Icons/Algood_Gul.svg\" width=\"50%\" height=\"50%\">";
         break;
     case "House Allyrion of Godsgrace":
-        //var el = document.getElementById("yabanner");
-        //el.innerHTML="<img src=\"./Bilde/Icons/Hand_Dark_Red.svg\" width=\"100%\" height=\"50%\">";
+        var el = document.getElementById("player1");
+        el.innerHTML="<img src=\"./Bilde/Icons/Hand_Dark_Red.svg\" width=\"50%\" height=\"50%\">";
         break;
-    case "House Allyrion of Godsgrace":
-        //var el = document.getElementById("yabanner");
-        // el.innerHTML="<img src=\"./Bilde/Icons/Hand_Dark_Red.svg\" width=\"100%\" height=\"50%\">";
-        break;
+
     case "House Amber":
-        //var el = document.getElementById("yabanner");
-        //el.innerHTML="<img src=\"./Bilde/Icons/Hand_Dark_Red.svg\" width=\"100%\" height=\"50%\">";
+        var el = document.getElementById("player1");
+        el.innerHTML="<img src=\"./Bilde/Banner_Whater.svg\" width=\"50%\" height=\"50%\">";
         break;
     case "House Ambrose":
-        //var el = document.getElementById("yabanner");
-        //el.innerHTML="<img src=\"./Bilde/Icons/Hand_Dark_Red.svg\" width=\"100%\" height=\"50%\">";
+        var el = document.getElementById("player1");
+        el.innerHTML="<img src=\"./Bilde/Icons/Red_Mauer.svg\" width=\"50%\" height=\"50%\">";
         break;
     case "House Appleton of Appleton":
-        //var el = document.getElementById("yabanner");
-        //el.innerHTML="<img src=\"./Bilde/Icons/Hand_Dark_Red.svg\" width=\"100%\" height=\"50%\">";
+        var el = document.getElementById("player1");
+        el.innerHTML="<img src=\"./Bilde/Icons/Wood_Kind_Gray.svg\" width=\"50%\" height=\"50%\">";
         break;
     case "House Arryn of Gulltown":
-        //var el = document.getElementById("yabanner");
-        //el.innerHTML="<img src=\"./Bilde/Icons/Hand_Dark_Red.svg\" width=\"100%\" height=\"50%\">";
+        var el = document.getElementById("player1");
+        el.innerHTML="<img src=\"./Bilde/Icons/Blue_Hero.svg\" width=\"50%\" height=\"50%\">";
         break;
     case "House Arryn of the Eyrie":
-        //var el = document.getElementById("yabanner");
-        //el.innerHTML="<img src=\"./Bilde/Icons/Hand_Dark_Red.svg\" width=\"100%\" height=\"50%\">";
+        var el = document.getElementById("player1");
+        el.innerHTML="<img src=\"./Bilde/Icons/Blue_Hero.svg\" width=\"50%\" height=\"50%\">";
         break;
     case "House Ashford of Ashford":
-        //var el = document.getElementById("yabanner");
-        el.innerHTML = "<img src=\"./Bilde/Icons/Hand_Dark_Red.svg\" width=\"100%\" height=\"50%\">";
+        var el = document.getElementById("player1");
+        el.innerHTML = "<img src=\"./Bilde/Icons/Orang_son.svg\" width=\"50%\" height=\"50%\">";
         break;
     case "House Ashwood":
-        //var el = document.getElementById("yabanner");
-        el.innerHTML = "<img src=\"./Bilde/Icons/Hand_Dark_Red.svg\" width=\"100%\" height=\"50%\">";
+        var el = document.getElementById("player1");
+        el.innerHTML = "<img src=\"./Bilde/Icons/Red_Skog.svg\" width=\"50%\" height=\"50%\">";
         break;
     case "House Baelish of Harrenhal":
-        //var el = document.getElementById("yabanner");
-        //el.innerHTML="<img src=\"./Bilde/Icons/Hand_Dark_Red.svg\" width=\"100%\" height=\"50%\">";
+        var el = document.getElementById("player1");
+        el.innerHTML="<img src=\"./Bilde/Icons/Man_GReen_Fame.svg\" width=\"50%\" height=\"50%\">";
         break;
 
     default:
         console.log("work, no info")
 
+}
+
+//2 henter ut infor fra side nr 1 og legger det til i sede oversik slik at brukere er sikker på hvem de er
+//plaserers til venstere av siden
+
+switch (localStorage.getItem("player 1")) {
+    case "House Algood":
+        var el = document.getElementById("player_1");
+        el.innerHTML="<img src=\"./Bilde/Icons/Algood_Gul.svg\" width=\"50%\" height=\"50%\">";
+        break;
+    case "House Allyrion of Godsgrace":
+        var el = document.getElementById("player_1");
+        el.innerHTML="<img src=\"./Bilde/Icons/Hand_Dark_Red.svg\" width=\"50%\" height=\"50%\">";
+        break;
+
+    case "House Amber":
+        var el = document.getElementById("player_1");
+        el.innerHTML="<img src=\"./Bilde/Banner_Whater.svg\" width=\"50%\" height=\"50%\">";
+        break;
+    case "House Ambrose":
+        var el = document.getElementById("player_1");
+        el.innerHTML="<img src=\"./Bilde/Icons/Red_Mauer.svg\" width=\"50%\" height=\"50%\">";
+        break;
+    case "House Appleton of Appleton":
+        var el = document.getElementById("player_1");
+        el.innerHTML="<img src=\"./Bilde/Icons/Wood_Kind_Gray.svg\" width=\"50%\" height=\"50%\">";
+        break;
+    case "House Arryn of Gulltown":
+        var el = document.getElementById("player_1");
+        el.innerHTML="<img src=\"./Bilde/Icons/Blue_Hero.svg\" width=\"50%\" height=\"50%\">";
+        break;
+    case "House Arryn of the Eyrie":
+        var el = document.getElementById("player_1");
+        el.innerHTML="<img src=\"./Bilde/Icons/Blue_Hero.svg\" width=\"50%\" height=\"50%\">";
+        break;
+    case "House Ashford of Ashford":
+        var el = document.getElementById("player_1");
+        el.innerHTML = "<img src=\"./Bilde/Icons/Orang_son.svg\" width=\"50%\" height=\"50%\">";
+        break;
+    case "House Ashwood":
+        var el = document.getElementById("player_1");
+        el.innerHTML = "<img src=\"./Bilde/Icons/Red_Skog.svg\" width=\"50%\" height=\"50%\">";
+        break;
+    case "House Baelish of Harrenhal":
+        var el = document.getElementById("player_1");
+        el.innerHTML="<img src=\"./Bilde/Icons/Man_GReen_Fame.svg\" width=\"50%\" height=\"50%\">";
+        break;
+
+    default:
+        console.log("work, no info")
+
+}
+
+
+
+
+
+switch (localStorage.getItem("player 2")) {
+    case "House Algood":
+    var el = document.getElementById("player2");
+    el.innerHTML="<img src=\"./Bilde/Icons/Algood_Gul.svg\" width=\"50%\" height=\"50%\">";
+    break;
+case "House Allyrion of Godsgrace":
+    var el = document.getElementById("player2");
+    el.innerHTML="<img src=\"./Bilde/Icons/Hand_Dark_Red.svg\" width=\"50%\" height=\"50%\">";
+    break;
+
+case "House Amber":
+    var el = document.getElementById("player2");
+    el.innerHTML="<img src=\"./Bilde/Banner_Whater.svg\" width=\"50%\" height=\"50%\">";
+    break;
+case "House Ambrose":
+    var el = document.getElementById("player2");
+    el.innerHTML="<img src=\"./Bilde/Icons/Red_Mauer.svg\" width=\"50%\" height=\"50%\">";
+    break;
+case "House Appleton of Appleton":
+    var el = document.getElementById("player2");
+    el.innerHTML="<img src=\"./Bilde/Icons/Wood_Kind_Gray.svg\" width=\"50%\" height=\"50%\">";
+    break;
+case "House Arryn of Gulltown":
+    var el = document.getElementById("player2");
+    el.innerHTML="<img src=\"./Bilde/Icons/Blue_Hero.svg\" width=\"500%\" height=\"50%\">";
+    break;
+case "House Arryn of the Eyrie":
+    var el = document.getElementById("player2");
+    el.innerHTML="<img src=\"./Bilde/Icons/Blue_Hero.svg\" width=\"50%\" height=\"50%\">";
+    break;
+case "House Ashford of Ashford":
+    var el = document.getElementById("player2");
+    el.innerHTML = "<img src=\"./Bilde/Icons/Orang_son.svg\" width=\"50%\" height=\"50%\">";
+    break;
+case "House Ashwood":
+    var el = document.getElementById("player2");
+    el.innerHTML = "<img src=\"./Bilde/Icons/Red_Skog.svg\" width=\"50%\" height=\"50%\">";
+    break;
+case "House Baelish of Harrenhal":
+    var el = document.getElementById("player2");
+    el.innerHTML="<img src=\"./Bilde/Icons/Man_GReen_Fame.svg\" width=\"50%\" height=\"50%\">";
+    break;
+
+
+    default:
+        console.log("work, no info")
 }
 
 switch (localStorage.getItem("player 2")) {
     case "House Algood":
-        //      var el = document.getElementById("yabanner");
-        //    el.innerHTML="<img src=\"./Bilde/Icons/Hand_Dark_Red.svg\" width=\"100%\" height=\"50%\">";
-        break;
-    case "House Allyrion of Godsgrace":
-        //var el = document.getElementById("yabanner");
-        //el.innerHTML="<img src=\"./Bilde/Icons/Hand_Dark_Red.svg\" width=\"100%\" height=\"50%\">";
-        break;
-    case "House Allyrion of Godsgrace":
-        //var el = document.getElementById("yabanner");
-        //el.innerHTML="<img src=\"./Bilde/Icons/Hand_Dark_Red.svg\" width=\"100%\" height=\"50%\">";
-        break;
-    case "House Amber":
-        //var el = document.getElementById("yabanner");
-        //el.innerHTML="<img src=\"./Bilde/Icons/Hand_Dark_Red.svg\" width=\"100%\" height=\"50%\">";
-        break;
-    case "House Ambrose":
-        //var el = document.getElementById("yabanner");
-        //el.innerHTML="<img src=\"./Bilde/Icons/Hand_Dark_Red.svg\" width=\"100%\" height=\"50%\">";
-        break;
-    case "House Appleton of Appleton":
-        //var el = document.getElementById("yabanner");
-        //el.innerHTML="<img src=\"./Bilde/Icons/Hand_Dark_Red.svg\" width=\"100%\" height=\"50%\">";
-        break;
-    case "House Arryn of Gulltown":
-        //var el = document.getElementById("yabanner");
-        //el.innerHTML="<img src=\"./Bilde/Icons/Hand_Dark_Red.svg\" width=\"100%\" height=\"50%\">";
-        break;
-    case "House Arryn of the Eyrie":
-        //var el = document.getElementById("yabanner");
-        //el.innerHTML="<img src=\"./Bilde/Icons/Hand_Dark_Red.svg\" width=\"100%\" height=\"50%\">";
-        break;
-    case "House Ashford of Ashford":
-        //var el = document.getElementById("yabanner");
-        //el.innerHTML="<img src=\"./Bilde/Icons/Hand_Dark_Red.svg\" width=\"100%\" height=\"50%\">";
-        break;
-    case "House Ashwood":
-        var el = document.getElementById("yabanner");
-        el.innerHTML = "<img src=\"./Bilde/Icons/Hand_Dark_Red.svg\" width=\"100%\" height=\"50%\">";
-        break;
-    case "House Baelish of Harrenhal":
+    var el = document.getElementById("player_2");
+    el.innerHTML="<img src=\"./Bilde/Icons/Algood_Gul.svg\" width=\"50%\" height=\"50%\">";
+    break;
+case "House Allyrion of Godsgrace":
+    var el = document.getElementById("player_2");
+    el.innerHTML="<img src=\"./Bilde/Icons/Hand_Dark_Red.svg\" width=\"50%\" height=\"50%\">";
+    break;
 
-        console.log(myImage);
-        break;
+case "House Amber":
+    var el = document.getElementById("player2_");
+    el.innerHTML="<img src=\"./Bilde/Banner_Whater.svg\" width=\"50%\" height=\"50%\">";
+    break;
+case "House Ambrose":
+    var el = document.getElementById("player2_");
+    el.innerHTML="<img src=\"./Bilde/Icons/Red_Mauer.svg\" width=\"50%\" height=\"50%\">";
+    break;
+case "House Appleton of Appleton":
+    var el = document.getElementById("player_2");
+    el.innerHTML="<img src=\"./Bilde/Icons/Wood_Kind_Gray.svg\" width=\"50%\" height=\"50%\">";
+    break;
+case "House Arryn of Gulltown":
+    var el = document.getElementById("player_2");
+    el.innerHTML="<img src=\"./Bilde/Icons/Blue_Hero.svg\" width=\"500%\" height=\"50%\">";
+    break;
+case "House Arryn of the Eyrie":
+    var el = document.getElementById("player_2");
+    el.innerHTML="<img src=\"./Bilde/Icons/Blue_Hero.svg\" width=\"50%\" height=\"50%\">";
+    break;
+case "House Ashford of Ashford":
+    var el = document.getElementById("player_2");
+    el.innerHTML = "<img src=\"./Bilde/Icons/Orang_son.svg\" width=\"50%\" height=\"50%\">";
+    break;
+case "House Ashwood":
+    var el = document.getElementById("player_2");
+    el.innerHTML = "<img src=\"./Bilde/Icons/Red_Skog.svg\" width=\"50%\" height=\"50%\">";
+    break;
+case "House Baelish of Harrenhal":
+    var el = document.getElementById("player2");
+    el.innerHTML="<img src=\"./Bilde/Icons/Man_GReen_Fame.svg\" width=\"50%\" height=\"50%\">";
+    break;
+
 
     default:
         console.log("work, no info")
@@ -102,6 +200,14 @@ switch (localStorage.getItem("player 2")) {
 
 
 
+
+/////bytter farge på knapp når vedkommen trykker på den 
+
+//blue: #375DAA
+//green: 57883D
+
+
+document.getElementsByClassName("button_Disk ").bgcolor="#375DAA";
 
 
 
