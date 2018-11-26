29090bd066452eeab0ae18232f3c2ee254fc3260
Svenn Kristoffer Berg <32140076+skberg@users.noreply.github.com>
2018-11-26 20:13:48 +0100

    update
    adding button to navegatev after a set cards, and adding a 3 site for the winner

--- Semesteroppgave_Vol2/_Coding/JS/GAmes.js
+++ Semesteroppgave_Vol2/_Coding/JS/GAmes.js
@@ -1,3 +1,5 @@
+
+                                                            //plaserer på start 
 switch (localStorage.getItem("player 1")) {
     case "House Algood":
         var el = document.getElementById("player1");
@@ -46,9 +48,7 @@ switch (localStorage.getItem("player 1")) {
 
 }
 
-//2 henter ut infor fra side nr 1 og legger det til i sede oversik slik at brukere er sikker på hvem de er
-//plaserers til venstere av siden
-
+                                                            //plaserer på venstere under dice1
 switch (localStorage.getItem("player 1")) {
     case "House Algood":
         var el = document.getElementById("player_1");
@@ -97,10 +97,56 @@ switch (localStorage.getItem("player 1")) {
 
 }
 
+                                                            //plaserer på høyere av siden under dice2
+switch (localStorage.getItem("player 2")) {
+    case "House Algood":
+    var el = document.getElementById("player_2_v");
+    el.innerHTML="<img src=\"./Bilde/Icons/Algood_Gul.svg\" width=\"50%\" height=\"50%\">";
+    break;
+case "House Allyrion of Godsgrace":
+    var el = document.getElementById("player_2_v");
+    el.innerHTML="<img src=\"./Bilde/Icons/Hand_Dark_Red.svg\" width=\"50%\" height=\"50%\">";
+    break;
+
+case "House Amber":
+    var el = document.getElementById("player_2_v");
+    el.innerHTML="<img src=\"./Bilde/Banner_Whater.svg\" width=\"50%\" height=\"50%\">";
+    break;
+case "House Ambrose":
+    var el = document.getElementById("player_2_v");
+    el.innerHTML="<img src=\"./Bilde/Icons/Red_Mauer.svg\" width=\"50%\" height=\"50%\">";
+    break;
+case "House Appleton of Appleton":
+    var el = document.getElementById("player_2_v");
+    el.innerHTML="<img src=\"./Bilde/Icons/Wood_Kind_Gray.svg\" width=\"50%\" height=\"50%\">";
+    break;
+case "House Arryn of Gulltown":
+    var el = document.getElementById("player_2_v");
+    el.innerHTML="<img src=\"./Bilde/Icons/Blue_Hero.svg\" width=\"500%\" height=\"50%\">";
+    break;
+case "House Arryn of the Eyrie":
+    var el = document.getElementById("player_2_v");
+    el.innerHTML="<img src=\"./Bilde/Icons/Blue_Hero.svg\" width=\"50%\" height=\"50%\">";
+    break;
+case "House Ashford of Ashford":
+    var el = document.getElementById("player_2_v");
+    el.innerHTML = "<img src=\"./Bilde/Icons/Orang_son.svg\" width=\"50%\" height=\"50%\">";
+    break;
+case "House Ashwood":
+    var el = document.getElementById("player_2_v");
+    el.innerHTML = "<img src=\"./Bilde/Icons/Red_Skog.svg\" width=\"50%\" height=\"50%\">";
+    break;
+case "House Baelish of Harrenhal":
+    var el = document.getElementById("player_2_v");
+    el.innerHTML="<img src=\"./Bilde/Icons/Man_GReen_Fame.svg\" width=\"50%\" height=\"50%\">";
+    break;
 
 
+    default:
+        console.log("work, no info")
+}
 
-
+                                                            //plaserer på starts linje 
 switch (localStorage.getItem("player 2")) {
     case "House Algood":
     var el = document.getElementById("player2");
@@ -124,7 +170,7 @@ case "House Appleton of Appleton":
     el.innerHTML="<img src=\"./Bilde/Icons/Wood_Kind_Gray.svg\" width=\"50%\" height=\"50%\">";
     break;
 case "House Arryn of Gulltown":
-    var el = document.getElementById("player2");
+    var el = document.getElementById("player_2");
     el.innerHTML="<img src=\"./Bilde/Icons/Blue_Hero.svg\" width=\"500%\" height=\"50%\">";
     break;
 case "House Arryn of the Eyrie":
@@ -149,104 +195,136 @@ case "House Baelish of Harrenhal":
         console.log("work, no info")
 }
 
-switch (localStorage.getItem("player 2")) {
-    case "House Algood":
-    var el = document.getElementById("player_2");
-    el.innerHTML="<img src=\"./Bilde/Icons/Algood_Gul.svg\" width=\"50%\" height=\"50%\">";
-    break;
-case "House Allyrion of Godsgrace":
-    var el = document.getElementById("player_2");
-    el.innerHTML="<img src=\"./Bilde/Icons/Hand_Dark_Red.svg\" width=\"50%\" height=\"50%\">";
-    break;
-
-case "House Amber":
-    var el = document.getElementById("player2_");
-    el.innerHTML="<img src=\"./Bilde/Banner_Whater.svg\" width=\"50%\" height=\"50%\">";
-    break;
-case "House Ambrose":
-    var el = document.getElementById("player2_");
-    el.innerHTML="<img src=\"./Bilde/Icons/Red_Mauer.svg\" width=\"50%\" height=\"50%\">";
-    break;
-case "House Appleton of Appleton":
-    var el = document.getElementById("player_2");
-    el.innerHTML="<img src=\"./Bilde/Icons/Wood_Kind_Gray.svg\" width=\"50%\" height=\"50%\">";
-    break;
-case "House Arryn of Gulltown":
-    var el = document.getElementById("player_2");
-    el.innerHTML="<img src=\"./Bilde/Icons/Blue_Hero.svg\" width=\"500%\" height=\"50%\">";
-    break;
-case "House Arryn of the Eyrie":
-    var el = document.getElementById("player_2");
-    el.innerHTML="<img src=\"./Bilde/Icons/Blue_Hero.svg\" width=\"50%\" height=\"50%\">";
-    break;
-case "House Ashford of Ashford":
-    var el = document.getElementById("player_2");
-    el.innerHTML = "<img src=\"./Bilde/Icons/Orang_son.svg\" width=\"50%\" height=\"50%\">";
-    break;
-case "House Ashwood":
-    var el = document.getElementById("player_2");
-    el.innerHTML = "<img src=\"./Bilde/Icons/Red_Skog.svg\" width=\"50%\" height=\"50%\">";
-    break;
-case "House Baelish of Harrenhal":
-    var el = document.getElementById("player2");
-    el.innerHTML="<img src=\"./Bilde/Icons/Man_GReen_Fame.svg\" width=\"50%\" height=\"50%\">";
-    break;
-
-
-    default:
-        console.log("work, no info")
-}
 
 
 
 
 
-/////bytter farge på knapp når vedkommen trykker på den 
 
 //blue: #375DAA
 //green: 57883D
 
-
+/*
 document.getElementsByClassName("button_Disk ").bgcolor="#375DAA";
+*/
+
+var start = 1;
+var begin = 1;
 
 
 
-
-
-function rollDice() {
+function rollDice() {                                          //dice player 1
     var die1 = document.getElementById("die1");
     var d1 = Math.floor(Math.random() * 6) + 1;
     die1.innerHTML = d1;
+    bevegdegforfaen(d1);
+}
+
+
+
+function rollDice_2() {                                     //dice player 2
+    var die2 = document.getElementById("die2");
+    var d2 = Math.floor(Math.random() * 6) + 1;
+    die2.innerHTML = d2;
+    bevegdegforfaen(d2);
+}
+
+
+
+////Hjelp!!!!!!!11
+
+function bevegdegforfaen(d1,d2){
+  
+    var spillertur = 1;
+    var player1 = document.getElementById("player1");
+    var player2 = document.getElementById("player2");
+    if( spillertur == 1 ){
+        spillertur = 2;
+    } else {
+        spillertur = 2;
+    }
+    var brikke = document.getElementById(start);
+    var brikke2 = document.getElementById(begin);
+    
+    brikke.style.backgroundColor ="white";                  //Card Standar farge 
+    
+    for(i = 0; i < d1; i++){
+        start = start + 1;
+        begin = begin + 1;
+        brikke = document.getElementById(start);
+        brikke2 = document.getElementById(begin);
+        brikke2.appendChild(player2)
+        brikke.appendChild(player1);
+   
+    }
+
+    brikke.style.backgroundColor = "#375DAA";               //independet color after players 
 }
 
 
 //hjelp!!!!
-
+/*
 function move_img(player2) {
     var step=50; // change this to different step value
+    
     switch(player2){
+
     case "down":
-    var x=document.getElementById('player 2').offsetTop;
+    var x=document.getElementById('player2').offsetTop;
     x= x + step;
-    document.getElementById('player 2').style.top= x + "px";
+    document.getElementById('player2').style.top= x + "id";
     break;
     
     case "up":
-    var x=document.getElementById('player 2').offsetTop;
+    var x=document.getElementById('player2').offsetTop;
     x= x -step;
-    document.getElementById('player 2').style.top= x + "px";
+    document.getElementById('player2').style.top= x + "id";
     break;
     
     case "left":
-    var y=document.getElementById('player 2').offsetLeft;
+    var y=document.getElementById('player2').offsetLeft;
     y= y - step;
-    document.getElementById('player 2').style.left= y + "px";
+    document.getElementById('player2').style.left= y + "id";
     break;
     
     case "right":
-    var y=document.getElementById('player 2').offsetLeft;
+    var y=document.getElementById('player2').offsetLeft;
     y= y + step;
-    document.getElementById('player 2').style.left= y + "px";
+    document.getElementById('player2').style.left= y + "id";
     break;
+
     }
+
+
+
     }
+
+    var game_board = [
+        //kolonne 0, 1, 2, 3
+        ["End",50,'#'],                       // 0 rad
+        [48,47,'#',45],                    // 1
+        [44,'#',42,41],                     // 2
+        [40,39,38,37],                     // 3
+        [36,'#','#',33],                     // 4
+        [32,'#',30,'#'],                     // 5
+        [28,27,26,25],                     // 6
+        ['#','#',22,21],                     // 7
+        [20,19,18, '#'],                   // 8
+    ]
+game_board[player_pos[2],player_pos[20]]
+
+// player_pos[rad, kolonne]
+var player_pos = [8,2]  
+function game_board(){
+
+};
+*/
+
+
+
+//spørsmål for mogrgen dagen 
+
+//1 hvordan plasere data hvor jeg vil med hjelp av arrayen vi lagde i går 
+// implumentere bevegelse med W,A,S,D & arow up and down and horisontalt 
+// så dice 2 til å BARE flytte player 2 
+// sentrere logo. 
